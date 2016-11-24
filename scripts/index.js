18c6cfafaa3927b8d215381ebe37a343e724b1be
level5hm@gmail.com <level5hm@gmail.com>
2016-11-24 12:31:24 +0300

    Added notifications and adding books

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,74 @@
+//notifications
+var activityList = document.getElementsByClassName("menu-block__activity")[0];
+
+var timeStamps = [];
+
+setInterval(updateTimeStamps, 1000);
+
+function updateTimeStamps() {
+    timeStamps.forEach(function(stamp) {
+        stamp.innerHTML = "";
+        stamp.minutes++;
+        if (stamp.minutes >= 60) {
+            stamp.minutes = 0;
+            stamp.hours++;
+        }
+        if (stamp.hours > 0) {
+            stamp.innerHTML += stamp.hours + " hours ";
+        }
+        stamp.innerHTML += stamp.minutes + " minutes ago";
+    }, this);
+}
+
+function pushNotification() {
+    var notification = document.createElement("li"); //wrapper
+    notification.className = "menu-block__item";
+    var icon = document.createElement("i"); //icon
+    icon.className = "menu-block__pic menu-block__pic_top fa fa-clock-o";
+    var body = document.createElement("div");
+    body.className = "menu-block__body"; //body
+
+    //deciding whether the notification text starts with bright or with pale text
+    if (typeof arguments[0] === "boolean"){
+        if (arguments[0] === true) {
+            startWithBright = 1;
+        }
+        else {
+            startWithBright = 0;
+        }
+    }
+
+    //assigning text bright or pale
+    for (var i = 1; i < arguments.length; i++) {
+        var text = document.createElement("span");
+        if (i % 2 != startWithBright) {
+            text.className = "menu-block__text_pale"; //pale text
+        }
+        else {
+            text.className = "menu-block__text"; //bright text
+        }
+        text.innerHTML = arguments[i];
+        body.appendChild(text);
+    }
+
+    var timeStamp = document.createElement("span");
+    timeStamp.className = "menu-block__timestamp"; //time
+
+    timeStamp.innerHTML = "right now";
+    timeStamp.minutes = 0;
+    timeStamp.hours = 0;
+    timeStamps.push(timeStamp);
+
+    //appending all the children
+    activityList.insertBefore(notification, activityList.firstChild);
+    notification.appendChild(icon);
+    notification.appendChild(body);
+    body.appendChild(timeStamp);
+
+    if (activityList.children.length > 5) {
+        activityList.removeChild(activityList.lastChild);
+    }
+}
 
 Element.prototype.getChildByClass = function (childClass) {
     for (var i = 0; i < this.childNodes.length; i++) {
@@ -125,12 +196,18 @@ for (var i = 0; i < searchFields.length; i++) {
 
     searchField.addEventListener("input", addSearchFunction("book__name", false));
     searchField.addEventListener("input", addSearchFunction("book__author", false));
+    searchField.addEventListener("blur", notify);
 }
 
 function addSearchFunction(searchClass, strict) {
     return function() {
         addBookFilter(searchClass, this.value, strict, "findSymbols");
     } 
+}
+
+function notify() {
+    if (this.value != "")
+        pushNotification(false, "Searching ", this.value, " in books");
 }
 
 
@@ -144,10 +221,19 @@ for (var i = 0; i < filterButtons.length; i++) {
     filterButton.addEventListener("click", highlightButton);
     
     switch (filterButton.innerHTML) {
+        case "All Books":
+            filterButton.addEventListener("click",function() {
+                pushNotification(false, " Showing ", " all books")
+            });
+        break;
         case "Most Recent":
+            filterButton.addEventListener("click", addRecentFilter);
         break;
         case "Most Popular":
             filterButton.addEventListener("click", addMostPopularFilter);
+        break;
+        case "Free Books":
+            filterButton.addEventListener("click", addFreeFilter);
         break;
     }
 }
@@ -157,15 +243,36 @@ function highlightButton() {
         filterButtons[i].className = "filters__item";
     }
     removeMostPopularFilter();
+    removeRecentFilter();
+    removeFreeFilter();
     this.className = "filters__item filters__item_highlighted";
 }
 
 function addMostPopularFilter() {
     addBookFilter("book__rating", "5", true, "equalTo");
+    pushNotification(false, " Showing ", " popular books");
 }
 
 function removeMostPopularFilter() {
     removeBookFilter("book__rating");
+}
+
+function addRecentFilter() {
+    //addBookFilter("book__rating", "5", true, "equalTo");
+    pushNotification(false, " Showing ", " recent books");
+}
+
+function removeRecentFilter() {
+    //removeBookFilter("book__rating");
+}
+
+function addFreeFilter() {
+    //addBookFilter("book__rating", "5", true, "equalTo");
+    pushNotification(false, " Showing ", " free books");
+}
+
+function removeFreeFilter() {
+    //removeBookFilter("book__rating");
 }
 
 //updating book ratings
@@ -175,18 +282,24 @@ updateBookRatings();
 
 function updateBookRatings() {
     for (var i = 0; i < books.length; i++) {
-        updateRating(books[i]);
+        updateRating(books[i], books[i].getChildByClass("book__rating").innerHTML, false);
     }
 }
 
-function updateRating(book) {
+function updateRating(book, rating, sendNotification) {
     var ratingSymbols = book.getChildByClass("rating");
     while (ratingSymbols.firstChild) {
         ratingSymbols.removeChild(ratingSymbols.firstChild);
     }
-    var rating = book.getChildByClass("book__rating").innerHTML;
+
     for (var j = 0; j < 5; j++) {
-        createStar(j);
+        var star = createStar(j);
+        star.index = j + 1;
+        star.addEventListener("click", function() {
+            var book = this.parentNode.parentNode;
+            book.getChildByClass("book__rating").innerHTML = this.index;
+            updateRating(book, this.index, true);
+        });
     }
 
     function createStar(index) {
@@ -200,5 +313,93 @@ function updateRating(book) {
         else
             star.className = "rating__pic fa fa-star";
         ratingSymbols.appendChild(star);
+        return star;
     }
+
+    if (sendNotification)
+        pushNotification(true, book.getChildByClass("book__name").innerHTML, " by ", 
+                book.getChildByClass("book__author").innerHTML, " rated ", rating + " / 5");
+}
+
+
+//adding books
+
+var addBookButton = document.getElementsByClassName("menu-block__button")[0];
+addBookButton.addEventListener("click", addBook);
+
+var newPicture = document.getElementsByClassName("menu-block__book-picture")[0];
+var pictureSrc = "images/jamie.jpg";
+
+newPicture.addEventListener("change", function() {
+    var file = this.files[0];
+
+    if (file.type.indexOf('image') < 0) {
+        pictureSrc = "images/jamie.jpg";
+        console.log(pictureSrc);
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function() {
+        pictureSrc = reader.result;
+    }
+    reader.readAsDataURL(file);
+})
+
+function changeColor(button, color) {
+    var prevColor = "#f2795a";
+    button.style.background = color;
+    setTimeout(function() {
+        button.style.background = prevColor;
+    }, 600);
+}
+
+function addBook() {
+    var newName = document.getElementsByClassName("menu-block__book-name")[0];
+    var newAuthor = document.getElementsByClassName("menu-block__book-author")[0];
+
+    if (newName.value == "" || newAuthor.value == "") {
+        changeColor(addBookButton, "red");
+        return;
+    }
+
+    changeColor(addBookButton, "greenyellow");
+    var bookList = document.getElementsByClassName("book-list")[0];
+    var newBook = document.createElement("div");
+    newBook.className = "book";
+
+    console.log(pictureSrc);
+
+    var image = document.createElement("img");
+    image.setAttribute("src", pictureSrc);
+    image.setAttribute("alt", "image");
+    image.className = "book__image";
+
+    var name = document.createElement("span");
+    name.className = "book__name";
+    name.innerHTML = newName.value;
+
+    var author = document.createElement("span");
+    author.className = "book__author";
+    author.innerHTML = newAuthor.value;
+
+    var rating = document.createElement("span");
+    rating.className = "book__rating";
+    rating.innerHTML = 0;
+
+    var ratingSymbols = document.createElement("ul");
+    ratingSymbols.className = "rating";
+
+    bookList.insertBefore(newBook, bookList.firstChild);
+    newBook.appendChild(image);
+    newBook.appendChild(name);
+    newBook.appendChild(author);
+    newBook.appendChild(rating);
+    newBook.appendChild(ratingSymbols);
+
+    updateRating(newBook, rating.innerHTML);
+
+    newName.value = "";
+    newAuthor.value = "";
+
+    pushNotification(false, "You added ", name.innerHTML, " by ", author.innerHTML, " to your ", "Must Read Titles");
 }
