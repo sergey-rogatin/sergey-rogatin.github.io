de5c69191b482764ba04039c5aef50320b3bd7c8
lvl5hm <level5hm@gmail.com>
2017-09-17 02:20:58 +0300

    added tables

--- js/game/resources/scenes/test.js
+++
@@ -1,5 +0,0 @@
-Engine.currScene = new Scene("testScene", 460, 320);
-//Engine.mainCamera.background = Loader.loadSprite("js/game/resources/sprites/gameOfLife.png");
-
-
-let control = oControl.instantiate(0, 0);
